acc34db2e56e30fbb48ce695eb7d5ad4b6354949
VaneDC <kamui713@gmail.com>
2018-04-18 18:25:34 -0300

    buscar productos

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\demo\documents\github\bunker\prototipobunker\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
